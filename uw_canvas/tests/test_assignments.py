126a39d8bedb5f13c4d0eaa9af53a863ffc21ac5
Jim Laney <jlaney@uw.edu>
2017-07-25 11:52:45 -0700

    adds external tool tags for assignments

--- uw_canvas/tests/test_assignments.py
+++ uw_canvas/tests/test_assignments.py
@@ -16,6 +16,7 @@ class CanvasTestAssignments(TestCase):
         self.assertEquals(assignment.grading_standard_id, None, "Grading Standard ID")
         self.assertEquals(assignment.turnitin_enabled, False, "turnitin enabled")
         self.assertEquals(assignment.vericite_enabled, True, "vericite enabled")
+        self.assertEquals(assignment.submission_types, ['online_text_entry', 'online_url'])
 
     def test_assignment_by_course_sis_id(self):
         canvas = Assignments()
@@ -26,6 +27,21 @@ class CanvasTestAssignments(TestCase):
         canvas = Assignments(as_user="730FA4DCAE3411D689DA0004AC494FFE")
         assignments = canvas.get_assignments_by_sis_id("2013-autumn-PHYS-248-A")
         self.assertEquals(len(assignments), 2, "Assignment Count")
+
+    def test_json_data(self):
+        canvas = Assignments()
+
+        assignments = canvas.get_assignments_by_sis_id("2013-autumn-PHYS-248-A")
+
+        assignment = assignments[0]
+        data = assignment.json_data()["assignment"]
+        self.assertEquals('external_tool' in assignment.submission_types, False)
+        self.assertEquals('external_tool_tag_attributes' in data, False)
+
+        assignment = assignments[1]
+        data = assignment.json_data()["assignment"]
+        self.assertEquals('external_tool' in assignment.submission_types, True)
+        self.assertEquals('external_tool_tag_attributes' in data, True)
 
     def test_due_at_none(self):
         data = {
