5abeed57b7f3259bd690983168cd1bf67e6382ab
Jim Laney <jlaney@uw.edu>
2017-10-09 15:42:13 -0700

    adds methods for converting canvas sis_id to sws id

--- uw_canvas/tests/test_sections.py
+++ uw_canvas/tests/test_sections.py
@@ -1,10 +1,37 @@
 from unittest import TestCase
 from uw_canvas.utilities import fdao_canvas_override
 from uw_canvas.sections import Sections
+from uw_canvas.models import CanvasSection
 
 
 @fdao_canvas_override
 class CanvasTestSections(TestCase):
+    def test_sis_id(self):
+        section = CanvasSection()
+        self.assertEquals(section.sws_section_id(), None)
+        self.assertEquals(section.sws_instructor_regid(), None)
+        self.assertEquals(section.is_academic_sis_id(), False)
+
+        section = CanvasSection(sis_section_id="2013-spring-PHYS-121-A--")
+        self.assertEquals(section.sws_section_id(), "2013,spring,PHYS,121/A")
+        self.assertEquals(section.sws_instructor_regid(), None)
+        self.assertEquals(section.is_academic_sis_id(), True)
+
+        section = CanvasSection(sis_section_id="2013-spring-PHYS-121-AB")
+        self.assertEquals(section.sws_section_id(), "2013,spring,PHYS,121/AB")
+        self.assertEquals(section.sws_instructor_regid(), None)
+        self.assertEquals(section.is_academic_sis_id(), True)
+
+        section = CanvasSection(sis_section_id="2013-spring-PHYS-599-A-9136CCB8F66711D5BE060004AC494FFE--")
+        self.assertEquals(section.sws_section_id(), "2013,spring,PHYS,599/A")
+        self.assertEquals(section.sws_instructor_regid(), "9136CCB8F66711D5BE060004AC494FFE")
+        self.assertEquals(section.is_academic_sis_id(), True)
+
+        section = CanvasSection(sis_section_id="course_123456_groups")
+        self.assertEquals(section.sws_section_id(), None)
+        self.assertEquals(section.sws_instructor_regid(), None)
+        self.assertEquals(section.is_academic_sis_id(), False)
+
     def test_sections(self):
         canvas = Sections()
 
