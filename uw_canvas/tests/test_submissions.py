e7b2b622ce35a33b9998b26066f29a872155f50f
Jim Laney <jlaney@uw.edu>
2019-04-19 09:49:13 -0700

    fix assignment parameter name for submissions api

--- uw_canvas/tests/test_submissions.py
+++ uw_canvas/tests/test_submissions.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from uw_canvas.utilities import fdao_canvas_override
 from uw_canvas.submissions import Submissions
+import mock
 
 
 @fdao_canvas_override
@@ -21,7 +22,7 @@ class CanvasTestSubmissions(TestCase):
     def test_submissions_by_course_id(self):
         canvas = Submissions()
         submissions = canvas.get_submissions_multiple_assignments(
-            False, "862539", "all")
+            False, "862539", students="all")
         self.assertEquals(len(submissions), 4, "Submission Count")
 
         sub = submissions[0]
@@ -36,5 +37,45 @@ class CanvasTestSubmissions(TestCase):
     def test_submission_by_sis_id(self):
         canvas = Submissions()
         submissions = canvas.get_submissions_multiple_assignments_by_sis_id(
-            False, "2013-autumn-PHYS-248-A", "all")
+            False, "2013-autumn-PHYS-248-A", students="all")
         self.assertEquals(len(submissions), 3, "Submission Count")
+
+    @mock.patch.object(Submissions, '_get_resource_url')
+    def test_get_submissions_multiple_assignments(self, mock_get):
+        canvas = Submissions()
+        result = canvas.get_submissions_multiple_assignments(
+            False, "12345", students=["111111", "222222"])
+        mock_get.assert_called_with(
+            '/api/v1/courses/12345/students/submissions?student_ids[]='
+            '111111&student_ids[]=222222', True, None)
+
+        result = canvas.get_submissions_multiple_assignments(
+            False, "12345", assignments=["4444444", "5555555"])
+        mock_get.assert_called_with(
+            '/api/v1/courses/12345/students/submissions?assignment_ids[]='
+            '4444444&assignment_ids[]=5555555', True, None)
+
+        result = canvas.get_submissions_multiple_assignments(
+            True, "123456", students=["333333", "777777"],
+            workflow_state="submitted")
+        mock_get.assert_called_with(
+            '/api/v1/sections/123456/students/submissions?student_ids[]='
+            '333333&student_ids[]=777777&workflow_state=submitted',
+            True, None)
+
+    @mock.patch.object(Submissions, '_get_resource_url')
+    def test_get_submissions_multiple_assignments_by_sis_id(self, mock_get):
+        canvas = Submissions()
+        result = canvas.get_submissions_multiple_assignments_by_sis_id(
+            False, "2013-autumn-PHYS-248-A", students=["111111", "222222"])
+        mock_get.assert_called_with(
+            '/api/v1/courses/sis_course_id%3A2013-autumn-PHYS-248-A/'
+            'students/submissions?student_ids[]=111111&student_ids[]=222222',
+            True, None)
+
+        result = canvas.get_submissions_multiple_assignments_by_sis_id(
+            True, "2013-autumn-PHYS-248-AB", students=["333333", "777777"])
+        mock_get.assert_called_with(
+            '/api/v1/sections/sis_section_id%3A2013-autumn-PHYS-248-AB/'
+            'students/submissions?student_ids[]=333333&student_ids[]=777777',
+            True, None)
