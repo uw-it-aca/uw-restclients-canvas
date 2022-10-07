41bb00ddbb4ad8e2b1757f940622a95108ab5caa
Jim Laney <jlaney@uw.edu>
2022-10-07 13:02:32 -0700

    update copyright

--- uw_canvas/tests/test_enrollments.py
+++ uw_canvas/tests/test_enrollments.py
@@ -1,5 +1,6 @@
-# Copyright 2021 UW-IT, University of Washington
+# Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
+
 
 from unittest import TestCase
 from uw_canvas.utilities import fdao_canvas_override
